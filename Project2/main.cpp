0f1a898940f19c36c7900a5f3b8ce7b45997045f
Мунтян Максим <maksimuntyan@gmail.com>
2024-10-04 01:53:23 +0300

    Добавьте файлы проекта.

---
+++ Project2/main.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+template <class X>
+class Matrix
+{
+	X** ptr;
+	int str;
+	int st;
+
+public:
+
+	Matrix()
+	{
+		ptr = nullptr;
+		str = st = 0;
+	}
+	Matrix(int _str, int _st)
+	{
+		str = _str;
+		st = _st;
+		ptr = new X * [str];
+
+		for (int i = 0; i < str; i++)
+		{
+			ptr[i] = new X[st]{ 0 };
+		}
+
+	}
+	void Input()
+	{
+		for (int i = 0; i < str; i++)
+		{
+			for (int j = 0; j < st; j++)
+			{
+				ptr[i][j] = rand() % 100;
+			}
+		}
+	}
+	void Print()const
+	{
+		for (int i = 0; i < str; i++)
+		{
+			for (int j = 0; j < st; j++)
+			{
+				cout << ptr[i][j] << "\t";
+			}
+			cout << endl << endl;
+		}
+	}
+	~Matrix()
+	{
+		for (int i = 0; i < str; i++)
+		{
+			delete[] ptr[i];
+		}
+		delete[] ptr;
+	}
+};
+
+int main()
+{
+	Matrix<char> obj1(3, 2);
+	obj1.Input();
+	obj1.Print();
+
+	Matrix<int> obj2(5, 10);
+	obj2.Input();
+	obj2.Print();
+
+	Matrix<double> obj3(1.5, 4.5);
+}
